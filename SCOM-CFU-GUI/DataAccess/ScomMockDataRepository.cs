2b0cc4f4adc3b57a7c2a654bf70123d5be27b204
yannick zwijsen <zwijse@gmail.com>
2020-03-15 18:34:17 +0100

    mock data will now always have the same guids to make it easier for future testing

--- SCOM-CFU-GUI/DataAccess/ScomMockDataRepository.cs
+++ SCOM-CFU-GUI/DataAccess/ScomMockDataRepository.cs
@@ -21,9 +21,10 @@ namespace SCOM_CFU_GUI.DataAccess
             await Task.Delay(2000);
 
             List<ScomGroup> groupList = new List<ScomGroup>();
-            groupList.Add(new ScomGroup(Guid.NewGuid(), "Windows 2016 Group"));
-            groupList.Add(new ScomGroup(Guid.NewGuid(), "Windows 2012 Group"));
-            groupList.Add(new ScomGroup(Guid.NewGuid(), "Some Test Group"));
+
+            groupList.Add(new ScomGroup(new Guid("b564aa9d2d674d05a3177658df88ce79"), "Windows 2016 Group"));
+            groupList.Add(new ScomGroup(new Guid("f968f6d7da904fe2a75d6ae0af8eecf3"), "Windows 2012 Group"));
+            groupList.Add(new ScomGroup(new Guid("9704a190648c455fb84381a4c91a1bce"), "Some Test Group"));
 
             return groupList;
         }
@@ -34,40 +35,57 @@ namespace SCOM_CFU_GUI.DataAccess
 
             List<ScomMP> mpList = new List<ScomMP>();
             List<ScomTarget> targetList = new List<ScomTarget>();
-            List<ScomWorkflow> workflowList;
-
-            //create first dummy mp
-            var mpId = new Guid();
-            var mpName = "Windows 2016 Mp";
-            workflowList = CreateDummyWorkflows(8, "windows test workflow");
-            targetList.Add(new ScomTarget(Guid.NewGuid(), "Target A", mpId, new ObservableCollection<ScomWorkflow>(workflowList)));
-            workflowList = CreateDummyWorkflows(3, "other windows test workflow");
-            targetList.Add(new ScomTarget(Guid.NewGuid(), "Target B", mpId,  new ObservableCollection<ScomWorkflow>(workflowList)));
-            mpList.Add(new ScomMP(mpId, mpName, new ObservableCollection<ScomTarget>(targetList)));
-
-            //create second dummy mp
-            mpId = new Guid();
-            mpName = "Windows 2012 Mp";
-            workflowList = CreateDummyWorkflows(45, "some workflow");
-            targetList.Clear();
-            targetList.Add(new ScomTarget(Guid.NewGuid(), "test target", mpId, new ObservableCollection<ScomWorkflow>(workflowList)));
-            mpList.Add(new ScomMP(mpId, mpName, new ObservableCollection<ScomTarget>(targetList)));
-
-            return mpList;
-        }
-
-        private List<ScomWorkflow> CreateDummyWorkflows(int amount, string nameBase)
-        {
             List<ScomWorkflow> workflowList = new List<ScomWorkflow>();
 
-            for (int i = 0; i < amount; i++)
-            {
-                workflowList.Add(new ScomWorkflow(Guid.NewGuid(), $"{nameBase} Rule {i}", WorkflowType.Rule));
-                workflowList.Add(new ScomWorkflow(Guid.NewGuid(), $"{nameBase} Monitor {i}", WorkflowType.Monitor));
-                workflowCount += 2;
-            }
+            //create first dummy mp
+            var mpId = new Guid("c870e9207d344864b7283ce135180ef2");
+            var mpName = "Windows 2016 Mp";
 
-            return workflowList;
+            var targetId = new Guid("5e42b6ba571948b8975ad6bbd59bdd22");
+            var targetName = "Logical Disk";
+
+            workflowList.Add(new ScomWorkflow(new Guid("36d8369ddaf648d2a8c4e9e0d85d368a"), "Logical Drive Free Space", WorkflowType.Monitor));
+            workflowList.Add(new ScomWorkflow(new Guid("4345c647e8184a74b4b5c1d65b675618"), "Logical Drive Corrupted", WorkflowType.Monitor));
+
+            targetList.Add(new ScomTarget(targetId, targetName, mpId, new ObservableCollection<ScomWorkflow>(workflowList)));
+            mpList.Add(new ScomMP(mpId, mpName, new ObservableCollection<ScomTarget>(targetList)));
+
+            workflowList.Clear();
+            targetList.Clear();
+
+            //create 2nd dummy mp
+            mpId = new Guid("c1b8ecab4423411d84bdaf8d0afb2996");
+            mpName = "Windows 2012 Mp";
+
+            targetId = new Guid("347a7bf237ab4b2397698428d41660f4");
+            targetName = "Logical Disk";
+
+            workflowList.Add(new ScomWorkflow(new Guid("a3fef6952716485cb3fc82a956788e11"), "Logical Drive Free Space", WorkflowType.Monitor));
+            workflowList.Add(new ScomWorkflow(new Guid("e19bccbd528d45ac96b5ecc5a3625644"), "Logical Drive Corrupted", WorkflowType.Monitor));
+
+            targetList.Add(new ScomTarget(targetId, targetName, mpId, new ObservableCollection<ScomWorkflow>(workflowList)));
+            mpList.Add(new ScomMP(mpId, mpName, new ObservableCollection<ScomTarget>(targetList)));
+
+            workflowList.Clear();
+            targetList.Clear();
+
+            //create 3rd dummy mp
+            mpId = new Guid("4254fb437b1c4c24af082848450d75cc");
+            mpName = "Custom Service Monitoring";
+
+            targetId = new Guid("b5c6e16c0e954a87b764cc5189b115ae");
+            targetName = "Pulse Application";
+
+            workflowList.Add(new ScomWorkflow(new Guid("7a0dee22f0e04da2925660016e99b271"), "Pulse Service Monitor", WorkflowType.Monitor));
+            workflowList.Add(new ScomWorkflow(new Guid("aa6183397ed441a5bd4d0812a0d6b142"), "Pulse Error Event 455", WorkflowType.Rule));
+
+            targetList.Add(new ScomTarget(targetId, targetName, mpId, new ObservableCollection<ScomWorkflow>(workflowList)));
+            mpList.Add(new ScomMP(mpId, mpName, new ObservableCollection<ScomTarget>(targetList)));
+
+            workflowList.Clear();
+            targetList.Clear();
+
+            return mpList;
         }
 
         public string GetScomManagementGroupInfo()
